ea058b9f2b61576008dd9d2700433dc61351c9d7
whilefoo <whilefoo@users.noreply.github.com>
2025-03-18 17:56:37 +0100

    fix: log user

--- src/handlers/pull-reviewer.ts
+++ src/handlers/pull-reviewer.ts
@@ -167,7 +167,7 @@ export class PullReviewer {
     logger.info(`${repository.owner.login}/${repository.name}#${number} - ${action}`);
 
     if (payload.sender && (await this.isUserCollaborator(payload.sender.login))) {
-      logger.info("User is a collaborator, skipping review interval check, proceeding with review");
+      logger.info("User is a collaborator, skipping review interval check, proceeding with review", { user: payload.sender.login });
       return true;
     }
 
