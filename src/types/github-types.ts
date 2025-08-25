68707a204c09390bb3f0dc553b620175965d6f4f
whilefoo <whilefoo@users.noreply.github.com>
2025-08-25 22:34:46 +0200

    feat: remove old code

--- src/types/github-types.ts
+++
@@ -1,4 +0,0 @@
-import { RestEndpointMethodTypes } from "@octokit/rest";
-
-export type Issue = RestEndpointMethodTypes["issues"]["get"]["response"]["data"];
-export type CodeReviewStatus = RestEndpointMethodTypes["pulls"]["createReview"]["parameters"]["event"];
